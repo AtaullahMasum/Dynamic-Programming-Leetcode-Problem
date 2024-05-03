adf1bb761808d2a5f76a923d15e49c75af5587a0
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-05-03 10:55:35 +0600

    dp tabulation solution added

---  Frog Jump.py
+++  Frog Jump.py
@@ -47,3 +47,26 @@ def f(index, heights, dp):
 def frogJump(n: int, heights: List[int]) -> int:
     dp = [-1]*(n+1)
     return f(n-1, heights, dp)
+
+# Memoization convert into tabulation
+# Time Complexity is O(n)
+# Space Compexity is O(n)
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from typing import *
+
+  
+def frogJump(n: int, heights: List[int]) -> int:
+    dp = [0]*n
+    dp[0] = 0
+    for i in range(1, n):
+        fs = dp[i-1] +abs(heights[i] - heights[i-1])
+        ss = float('inf')
+        if i > 1:
+            ss = dp[i-2] + abs(heights[i] - heights[i-2])
+        dp[i] = min(fs, ss)
+    return dp[n-1]
+
