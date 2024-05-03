b1eda1bcf34c0e90ee3c155b46a270b0da148b5b
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-05-03 11:12:09 +0600

    space optimazation solution added

---  Frog Jump.py
+++  Frog Jump.py
@@ -69,4 +69,27 @@ def frogJump(n: int, heights: List[int]) -> int:
             ss = dp[i-2] + abs(heights[i] - heights[i-2])
         dp[i] = min(fs, ss)
     return dp[n-1]
+# Space Optimazation
+# Time Complexity is O(n)
+# Space Compexity is O(1)
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from typing import *
+
+  
+def frogJump(n: int, heights: List[int]) -> int:
+    prev1 = 0
+    prev2 = 0
+    for i in range(1, n):
+        fs = prev1 +abs(heights[i] - heights[i-1])
+        ss = float('inf')
+        if i > 1:
+            ss = prev2 + abs(heights[i] - heights[i-2])
+        curri = min(fs, ss)
+        prev2 = prev1
+        prev1 = curri
+    return prev1
 
