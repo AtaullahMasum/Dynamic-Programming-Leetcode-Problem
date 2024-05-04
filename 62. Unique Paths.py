00cc059ff7d5da1deda6faa97320f77013d67312
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-05-05 00:59:44 +0600

    Space Optimazation Solution added

--- 62. Unique Paths.py
+++ 62. Unique Paths.py
@@ -17,4 +17,26 @@ class Solution:
                         left = dp[i][j-1]
                     dp[i][j] = up + left
         return dp[-1][-1]
+# Space Optimization
+# Time Complexity is O(n*m)
+# Space Complexity is O(n)
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [0]*n
+       
+        for i in range(m):
+            temp = [0]*n
+            for j in range(n):
+                if i == 0 and j == 0:
+                    temp[j] = 1
+                    continue
+                else:
+                    up , left = 0,0
+                    if i > 0:
+                        up = dp[j]
+                    if j > 0:
+                        left = temp[j - 1]
+                    temp[j] = up + left
+            dp = temp           
+        return dp[-1]
         
