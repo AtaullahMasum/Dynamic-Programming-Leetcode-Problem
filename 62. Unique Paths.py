5afbb71135800f9add1426bd57a3f0c864e5a1c5
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-05-05 16:06:18 +0600

    combinatorics solution added

--- 62. Unique Paths.py
+++ 62. Unique Paths.py
@@ -39,4 +39,22 @@ class Solution:
                     temp[j] = up + left
             dp = temp           
         return dp[-1]
+# Using Combinatorics
+# Time Complexity is approximatelly O(m+n)
+# Space Complexity is O(1)
+class Solution():
+  def factorial(self, start, end):
+        result = 1
+        for i in range(start, end+1):
+            result *= i
+        return result
+  def uniquePaths(self, m, n):
+        
+        if m == 1 or n == 1:
+            return 1
+        maxn = max(m, n)
+        minn = min(m, n)
+        numerator = self.factorial(maxn, m + n - 2)
+        denominator = self.factorial(1, minn - 1)
+        return numerator // denominator
         
