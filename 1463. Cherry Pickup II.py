250fab982ca0db755af00f6d26b4c308fead95ca
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-05-08 21:17:04 +0600

    Tabulation method added

--- 1463. Cherry Pickup II.py
+++ 1463. Cherry Pickup II.py
@@ -57,4 +57,37 @@ class Solution:
         r, c = len(grid), len(grid[0])
         dp = [[[-1]*c for _ in range(c)] for _ in range(r)]
         return self.helper(0,0,c-1, r, c, grid, dp)
+# Tabulation Method 
+# Time Complexity is O(n*m*m*9)
+# Space Complexity is O(n*m*m)
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        n, m = len(grid), len(grid[0])
+        dp = [[[0]*m for _ in range(m)] for _ in range(n)]
+        # Base case 
+        for j1 in range(m):
+            for j2 in range(m):
+                if j1 == j2 :
+                    dp[n-1][j1][j2] = grid[n-1][j1]
+                else:
+                    dp[n-1][j1][j2] = grid[n-1][j1] + grid[n-1][j2]
+        for i in range(n-2, -1, -1):
+            for j1 in range(m):
+                for j2 in range(m):
+                    maxi = float('-inf')
+                    for dj1 in (-1, 0, 1):
+                        for dj2 in (-1, 0, 1):
+                            value = 0
+                            if j1 == j2:
+                                value = grid[i][j1]
+                            else:
+                                value = grid[i][j1] + grid[i][j2]
+                            if j1+dj1 < 0 or j1 + dj1 >= m or j2+dj2 < 0 or j2 + dj2 >= m:
+                                value += float('-inf')
+                            else:
+                                value += dp[i+1][j1+dj1][j2+dj2]
+                            maxi = max(maxi, value)
+                    dp[i][j1][j2] = maxi
+        return dp[0][0][m-1]
+        
         
