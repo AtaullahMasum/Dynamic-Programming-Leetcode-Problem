a70b66e27935c2c46b793c97f8e462d6075adc09
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-05-09 20:08:00 +0600

    Some Updated of these code

--- 698. Partition to K Equal Sum Subsets.py
+++ 698. Partition to K Equal Sum Subsets.py
@@ -1,5 +1,5 @@
 # Using Backtracking 
-# Time Complexity is O(nlogn + 2^n.n)
+# Time Complexity is O(nlogn + 2^n.k)
 # Space Complexity is O(n)
 class Solution:
     def helper(self, start_index, current_sum, target, nums, remaining_subsets, visited):
@@ -8,17 +8,20 @@ class Solution:
         if current_sum == target:
             return self.helper(0,0, target, nums, remaining_subsets - 1, visited)
         for i in range(start_index, len(nums)):
+            if visited[i] or current_sum + nums[i] > target:
+                continue
             if not visited[i] and current_sum + nums[i] <= target:
                 visited[i] = True
                 if self.helper(i+1, current_sum + nums[i], target, nums, remaining_subsets, visited):
                     return True
                 visited[i] = False
-                if current_sum == 0 or current_sum + nums[i] == target:
-                    break
+            # Early pruning
+            if current_sum == 0 or current_sum + nums[i] == target:
+                    break        
         return False
     
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
-        nums.sort()
+        nums.sort(reverse=True)
         total_sum = 0
         for i in range(len(nums)):
             total_sum += nums[i]
