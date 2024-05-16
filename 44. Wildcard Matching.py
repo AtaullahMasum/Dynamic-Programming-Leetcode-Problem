40992ab8c9429bd3c87f92b041021a91d3774ded
AtaullahMasum <34019692+AtaullahMasum@users.noreply.github.com>
2024-05-16 14:23:15 +0600

    all four solution added

--- 44. Wildcard Matching.py
+++ 44. Wildcard Matching.py
@@ -79,3 +79,31 @@ class Solution:
                 else:
                     dp[i][j] = False
         return dp[m][n]
+# Space Optimization 
+# Time Complexity is O(n*m)
+# Space Complexity is O(n)
+class Solution:
+
+    def isMatch(self, s: str, p: str) -> bool:
+        m = len(p)
+        n = len(s)
+        prev, curr = [False]*(n+1) , [False]*(n+1) 
+        prev[0] = True
+        for j in range(1, n+1):
+            prev[j] = False      
+        for i in range(1, m+1):
+            flag = True
+            for i1 in range(1, i+1):
+                if p[i1-1] != "*":
+                    flag = False
+                    break
+            curr[0] = flag
+            for j in range(1, n+1):
+                if p[i-1] == s[j-1] or p[i-1] == "?":
+                    curr[j] = prev[j-1]
+                elif p[i-1] == "*":
+                    curr[j] = prev[j] or curr[j-1]
+                else:
+                    curr[j] = False
+            prev = curr[:]
+        return prev[n]
